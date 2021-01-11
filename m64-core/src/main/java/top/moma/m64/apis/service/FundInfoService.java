2fd16640052127db056037b53f73812b64b957d5
moma-tech <ivandjp@gmail.com>
2021-01-11 17:56:33 +0800

    add CallHelper to convert response into Object

--- m64-core/src/main/java/top/moma/m64/apis/service/FundInfoService.java
+++ m64-core/src/main/java/top/moma/m64/apis/service/FundInfoService.java
@@ -3,14 +3,12 @@ package top.moma.m64.apis.service;
 import okhttp3.ResponseBody;
 import org.springframework.stereotype.Service;
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import top.moma.m64.apis.ApiURIConstants;
+import top.moma.m64.apis.caller.CallerHelper;
 import top.moma.m64.apis.caller.FundInfoCaller;
 import top.moma.m64.apis.entity.vo.params.FundInfoParams;
 import top.moma.m64.apis.entity.vo.response.FundInfoResponse;
-import top.moma.m64.apis.entity.vo.response.TestResponse;
 import top.moma.m64.core.helper.bean.BeanHelper;
 import top.moma.m64.core.helper.json.JsonHelper;
 
@@ -49,24 +47,30 @@ public class FundInfoService {
     FundInfoResponse fundInfoResponse = null;
     Map<String, String> a = BeanHelper.beanToStringMap(fundInfoParams);
     Call<ResponseBody> b = fundInfoCaller.getFundInfo(a);
-    b.enqueue(
-        new Callback<ResponseBody>() {
-          @Override
-          public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-            try {
-              // System.out.println(response.body().string());
-              TestResponse testResponse =
-                  JsonHelper.readValue(response.body().string(), TestResponse.class);
-              System.out.println(testResponse.getData().getCode());
-            } catch (IOException e) {
-              e.printStackTrace();
-            }
-          }
+    try {
+      FundInfoResponse testResponse = CallerHelper.getResponse(b, FundInfoResponse.class);
+      System.out.println(testResponse.getMessage());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    /*   b.enqueue(
+    new Callback<ResponseBody>() {
+      @Override
+      public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+        try {
+          // System.out.println(response.body().string());
+          TestResponse testResponse =
+              JsonHelper.readValue(response.body().string(), TestResponse.class);
+          System.out.println(testResponse.getData().getCode());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
 
-          @Override
-          public void onFailure(Call<ResponseBody> call, Throwable t) {
-            t.printStackTrace();
-          }
-        });
+      @Override
+      public void onFailure(Call<ResponseBody> call, Throwable t) {
+        t.printStackTrace();
+      }
+    });*/
   }
 }
