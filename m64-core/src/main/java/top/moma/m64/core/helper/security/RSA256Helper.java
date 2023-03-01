9bc4ba02fec7b73d88010124943f3aa80c5f8aa6
jipeng.dong <jipeng.dong@opay-inc.com>
2023-03-01 18:31:46 +0800

    Feature<RSA>: 增加判断

--- m64-core/src/main/java/top/moma/m64/core/helper/security/RSA256Helper.java
+++ m64-core/src/main/java/top/moma/m64/core/helper/security/RSA256Helper.java
@@ -1,5 +1,6 @@
 package top.moma.m64.core.helper.security;
 
+import top.moma.m64.core.helper.ObjectHelper;
 import top.moma.m64.core.helper.codec.Base64Decoder;
 import top.moma.m64.core.helper.codec.Base64Encoder;
 import top.moma.m64.core.helper.security.rsa256.RSA256Key;
@@ -7,6 +8,7 @@ import top.moma.m64.core.helper.security.rsa256.RSA256Key;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.HashMap;
@@ -25,18 +27,17 @@ public class RSA256Helper {
   public static final String KEY_ALGORITHM = "RSA";
   private static final String PUBLIC_KEY = "RSAPublicKey";
   private static final String PRIVATE_KEY = "RSAPrivateKey";
-
   private static RSA256Key rsa256Key;
 
   // 获得公钥
-  public static String getPublicKey(Map<String, Object> keyMap){
+  public static String getPublicKey(Map<String, Object> keyMap) {
     // 获得map中的公钥对象 转为key对象
     Key key = (Key) keyMap.get(PUBLIC_KEY);
     // 编码返回字符串
     return encryptBASE64(key.getEncoded());
   }
 
-  public static String getPublicKey(RSA256Key rsa256Key){
+  public static String getPublicKey(RSA256Key rsa256Key) {
     // 获得map中的公钥对象 转为key对象
     Key key = rsa256Key.getRsaPublicKey();
     // 编码返回字符串
@@ -51,7 +52,7 @@ public class RSA256Helper {
     return encryptBASE64(key.getEncoded());
   }
   // 获得私钥
-  public static String getPrivateKey(RSA256Key rsa256Key)  {
+  public static String getPrivateKey(RSA256Key rsa256Key) {
     // 获得map中的私钥对象 转为key对象
     Key key = rsa256Key.getRsaPrivateKey();
     // 编码返回字符串
@@ -69,32 +70,29 @@ public class RSA256Helper {
   }
 
   // 使用KeyPairGenerator 生成公私钥，存放于map对象中
-  public static Map<String, Object> initKey() throws Exception {
-    /* RSA算法要求有一个可信任的随机数源 */
-    // 获得对象 KeyPairGenerator 参数 RSA 1024个字节
-    KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
-    keyPairGen.initialize(1024);
-
-    // 通过对象 KeyPairGenerator 生成密匙对 KeyPair
-    KeyPair keyPair = keyPairGen.generateKeyPair();
-
-    // 通过对象 KeyPair 获取RSA公私钥对象RSAPublicKey RSAPrivateKey
-    RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-    RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-    // 公私钥对象存入map中
+  public static Map<String, Object> initKey() {
     Map<String, Object> keyMap = new HashMap<>(2);
-    keyMap.put(PUBLIC_KEY, publicKey);
-    keyMap.put(PRIVATE_KEY, privateKey);
+    try {
+      /* RSA算法要求有一个可信任的随机数源 */
+      // 获得对象 KeyPairGenerator 参数 RSA 2048个字节
+      KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+      keyPairGen.initialize(2048);
+      // 通过对象 KeyPairGenerator 生成密匙对 KeyPair
+      KeyPair keyPair = keyPairGen.generateKeyPair();
+      // 公私钥对象存入map中
+      keyMap.put(PUBLIC_KEY, keyPair.getPublic());
+      keyMap.put(PRIVATE_KEY, keyPair.getPrivate());
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
     return keyMap;
   }
 
-  /**
-   * 获取公私钥
-   **/
-  public static synchronized RSA256Key getRSA256Key() throws Exception {
-    if (rsa256Key == null) {
+  /** 获取公私钥 */
+  public static synchronized RSA256Key getRSA256Key() {
+    if (ObjectHelper.isEmpty(rsa256Key)) {
       synchronized (RSA256Key.class) {
-        if (rsa256Key == null) {
+        if (ObjectHelper.isEmpty(rsa256Key)) {
           rsa256Key = new RSA256Key();
           Map<String, Object> map = initKey();
           rsa256Key.setRsaPrivateKey((RSAPrivateKey) map.get(RSA256Helper.PRIVATE_KEY));
